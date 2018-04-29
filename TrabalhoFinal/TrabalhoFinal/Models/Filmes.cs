b57d9f697fbe284af8c66be99ae072b42af5ab3f
Joao Nunes <aluno19989@ipt.pt>
2018-04-29 23:04:50 +0100

    inicio da bd

---
+++ TrabalhoFinal/TrabalhoFinal/Models/Filmes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrabalhoFinal.Models {
+    public class filme {
+        public filme() {
+
+                
+        }
+
+
+    }
+
+
+}
